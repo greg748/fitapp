7089085b072cc6353c0fa4888d3322ad3f56583c
Greg Bowne <greg@gregbowne.com>
2017-12-26 15:43:47 -0500

    adding base muscles sql to repo in case I need to rebuild

---
+++ sql/muscles.sql
@@ -0,0 +1,17 @@
+insert into muscles (name,region,workout_type) values 
+('abdominals','core','{1,2,3,4,5,6,7}'),
+('adductors','leg','{1,3}'),
+('biceps','arm','{1,2,4}'),
+('deltoids','shoulders','{1,2,6}'),
+('erector spinae','back','{1,2,3,4,5,6,7}'),
+('gastroc/soleus','leg','{1,3}'),
+('hamstrings','leg','{1,3}'),
+('lats','back','{1,2,5,6}'),
+('obliques','core','{1,2,3,4,5,6,7}'),
+('pectorals','chest','{1,2,4}'),
+('quads','leg','{1,3}'),
+('rhomboids','back','{1,2,5,6}'),
+('trapezius','back','{1,2,5,6}'),
+('triceps','arm','{1,2,5}'),
+('glutes','leg','{1,3}'),
+('serratus','core','{1,2,5,7}');
